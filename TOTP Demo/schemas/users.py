41273a18029fcc1eed83f55c7a028e8b04d47e18
phutruan <phutruan@live.com>
2023-06-11 20:32:15 +0700

    TOTP demo
    TOTP demo. Support signup and login

---
+++ TOTP Demo/schemas/users.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+    secret: str
+
+
+class ShowUser(BaseModel):
+    email: str
+
+    class Config:
+        orm_mode = True
