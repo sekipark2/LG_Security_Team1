24fdc6b3b317f21680699f001425766c59ff5d83
ByungChang Cha(bcha) <morer@naver.com>
2023-06-19 14:59:20 +0900

    Modify Login session based on hash_id, add REST APIs

--- TOTP Demo/routers/signup/forms.py
+++ TOTP Demo/routers/signup/forms.py
@@ -8,6 +8,9 @@ regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 PASSWORD_LENGTH = 10
 
+number = '0123456789'
+symbol = '~`! @#$%^&*()_-+={[}]|\:;"\'<,>.?/'
+
 
 class UserCreateForm:
     def __init__(self, request: Request):
@@ -16,12 +19,18 @@ class UserCreateForm:
         self.email: Optional[str] = None
         self.password: Optional[str] = None
         self.confirmPassword: Optional[str] = None
+        self.first_name: Optional[str] = None
+        self.last_name: Optional[str] = None
+        self.address: Optional[str] = None
 
     async def load_data(self):
         form = await self.request.form()
         self.email = form.get("email")
         self.password = form.get("password")
         self.confirmPassword = form.get("confirm_password")
+        self.first_name = form.get("first_name")
+        self.last_name = form.get("last_name")
+        self.address = form.get("address")
 
     async def is_valid(self):
         if not re.fullmatch(regex, self.email):
@@ -30,6 +39,8 @@ class UserCreateForm:
             self.errors.append("Password must be >= %d chars" % (PASSWORD_LENGTH))
         if self.password != self.confirmPassword:
             self.errors.append("Confirm Password does not match")
+        if not set(self.password).intersection(set(number)) or not set(self.password).intersection(set(symbol)):
+            self.errors.append("Password must include one or more numbers and symbols " + symbol)
         if not self.errors:
             return True
         return False
--- TOTP Demo/schemas/users.py
+++ TOTP Demo/schemas/users.py
@@ -7,10 +7,15 @@ class UserCreate(BaseModel):
     secret: str
     verified: bool
     fail_counter: int
+    first_name: str
+    last_name: str
+    address: str
 
 
 class ShowUser(BaseModel):
     email: str
+    first_name: str
+    last_name: str
 
     class Config:
         orm_mode = True
